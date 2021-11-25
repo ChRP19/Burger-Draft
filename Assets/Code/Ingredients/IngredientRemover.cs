a8c039f0e483df6f8894187bc72c65543a5aaa72
Roman Chirkov <r.tchirkov2010@yandex.ru>
2021-11-25 23:03:13 +0300

    Moved OnJointBreak from IngredientAdder

--- Assets/Code/Ingredients/IngredientRemover.cs
+++ Assets/Code/Ingredients/IngredientRemover.cs
@@ -6,14 +6,20 @@ namespace Code.Ingredients
     {
         private const string Barrier = "Barrier";
 
+        public IngredientAdder IngredientAdder;
         public FixedJoint FixedJoint;
 
         private void OnCollisionEnter(Collision other)
         {
             if (other.gameObject.CompareTag(Barrier) && FixedJoint != null)
             {
-                FixedJoint.breakTorque = 100f;
+                IngredientAdder.IngredientList.RemoveRandomLastIngredients();
             }
+        }
+        
+        private void OnJointBreak(float breakForce)
+        {
+            IngredientAdder.IngredientList.RemoveIngredient(gameObject);
         }
     }
 }
